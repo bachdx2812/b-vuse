96d85c9f62373e6ac6a08c1f629883e1b9a38289
bachdx2812 <bachdx.hut@gmail.com>
2024-04-04 17:17:22 +0700

    fix export

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import useBreadcrumb from "./modules/breadcrumb";
 import useQuery from "./modules/query";
+import useGoQuery from "./modules/goQuery";
 
-export { useBreadcrumb, useQuery };
+export { useBreadcrumb, useQuery, useGoQuery };
