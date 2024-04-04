e1824980a2858c684da23b964525e7cfd8ca2a9b
bachdx2812 <bachdx.hut@gmail.com>
2024-04-04 17:21:43 +0700

    use reactive for useGoQuery instead of ref

--- modules/goQuery.js
+++ modules/goQuery.js
@@ -1,20 +1,20 @@
-import { ref } from "vue-demi";
+import { reactive } from "vue-demi";
 
 import GoQueryInput from "../models/goQueryInput";
 
 export default function useGoQuery({ page = 1, perPage = 10, query = {} }) {
-  const goQueryInput = ref(new GoQueryInput(page, perPage, query));
+  const goQueryInput = reactive(new GoQueryInput(page, perPage, query));
 
   function resetQuery() {
     goQueryInput.value = new GoQueryInput(page, perPage, q);
   }
 
   function updatePage(page) {
-    goQueryInput.value.pagyInput.page = page;
+    goQueryInput.pagyInput.page = page;
   }
 
   function updatePerPage(perPage) {
-    goQueryInput.value.pagyInput.perPage = perPage;
+    goQueryInput.pagyInput.perPage = perPage;
   }
 
   function updateQuery(query) {
