a36dab04e958ae985f2645527fad2722971406dc
bachdx2812 <bachdx.hut@gmail.com>
2024-07-29 14:26:08 +0700

    export useGoList

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ import useBreadcrumb from "./modules/breadcrumb";
 import useQuery from "./modules/query";
 import useGoQuery from "./modules/goQuery";
 import useList from "./modules/list";
+import useGoList from "./modules/goList";
 
-export { useBreadcrumb, useQuery, useGoQuery, useList };
+export { useBreadcrumb, useQuery, useGoQuery, useList, useGoList };
